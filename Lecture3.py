130b6509d4c45c4cc1322663a481242b1236cded
pfrohn <91150762+pfrohn@users.noreply.github.com>
2022-02-21 10:11:07 -0500

    Add files via upload

---
+++ Lecture3.py
@@ -0,0 +1,26 @@
+'''
+print(1+2+\
+      3+4+5+\
+      6+7+8+9+10)
+greeting = 'Hello ' + 'World'
+print(greeting)
+fname=input('Please enter your first name ')
+lname=input('Now enter your last name ')
+name=fname + ' ' +lname
+print('Your full name is',name,sep='\t')
+print('Bye!')
+'''
+"""
+print('Monday\tTuesday\tWednesday\tThursday\tFriday\tSaturday\tSunday')
+print('He said \'Hi\'')
+print('He said \"Hi\"')
+print('The folder: C:\\CS175')
+"""
+name='Jay'
+val=1789.2346
+print(f'{val:10,.3f}')
+discount=0.5
+print(f'{discount:.1%}')
+num=1234
+print(f'{num:3}')
+print(f'His name is {name:<5}.')
